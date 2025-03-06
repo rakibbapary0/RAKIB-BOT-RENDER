e383e04797d18bb00284781f05f48be3da8d39fd
RAFI-019 <ok276839@gmail.com>
2025-03-07 00:31:52 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,36 +1,38 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const fs = require('fs');
 
 module.exports = {
   config: {
-    name: "sendfile",
-    aliases: ["file"],
-    version: "1.0",
-    author: "Mahir Tahsan",
+    name: "file",
+    version: "5.1.0",
+    author: "𝖠𝗋𝖸𝖺𝗇 🐔",
     countDown: 5,
-    role: 0,
+    role: 2,
     shortDescription: "Send bot script",
     longDescription: "Send bot specified file ",
-    category: "owner",
-    guide: "{pn} file name. Ex: .{pn} filename"
+    category: "ARAFAT",
+    guide: "{pn} file name."
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61559946582981",];
-    if (!permission.includes(event.senderID)) {
-      return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
-    }
-
     const fileName = args[0];
+    const permission = global.GoatBot.config.vipUser;
+ if (!permission.includes(event.senderID)) {
+ api.sendMessage("❌ | Only Arafat's boss user can use the command", event.threadID, event.messageID);
+ return;
+			}
     if (!fileName) {
-      return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+      return api.sendMessage("😛 Please provide a file name.", event.threadID, event.messageID);
     }
 
-    const filePath = __dirname + `/${fileName}.js`;
-    if (!fs.existsSync(filePath)) {
+    const fileArYan = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(fileArYan)) {
       return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
     }
 
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const fileContent = fs.readFileSync(fileArYan, 'utf8');
     api.sendMessage({ body: fileContent }, event.threadID);
   }
 };
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
